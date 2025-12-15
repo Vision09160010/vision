5b81737e22d418aae0bc4608b2db64c3754a5ea2
vision <vision360437441@outlook.com>
2025-12-15 09:55:43 +0800

    update 功能更新

---
+++ base_rag/logs/log.py
@@ -0,0 +1,14 @@
+from loguru import logger
+
+logger.add("logs/myapp.log", format="{time:YYYY-MM-DD at HH:mm:ss} - {level} - {message}", rotation="500MB")
+
+logger.debug("调试信息")
+logger.info("正常信息")
+logger.warning("警告信息")
+logger.error("错误信息")
+logger.critical("严重错误信息")
+logger.level("DEBUG")
+
+
+
+
